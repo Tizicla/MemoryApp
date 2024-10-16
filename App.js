5ae59c72d09d7fa2db8d66f5150752856c05dbd8
Tizicla <mon.glez98@gmail.com>
2024-10-15 23:18:54 -0600

    Tags e info

--- App.js
+++ App.js
@@ -5,31 +5,135 @@ import Swiper from 'react-native-deck-swiper';
 
 const memorias = [
   {
-    nombre: 'DRAM😁',
-    capacidad: '4 GB',
+    nombre: 'RAM 😎',
+    capacidad: '4 GB - 64 GB',
     tipo: 'Memoria RAM',
-    fecha: '1968',
-    ventajas: 'Costo bajo y alta densidad.',
-    desventajas: 'Velocidad más baja que SRAM.',
+    velocidad: '2400 MT/s - 3200 MT/s',
+    fecha: '1950',
+    descripcion: 'Soy una memoria volátil que permite que tu dispositivo almacene datos temporales mientras esté encendido. ¡Ojo, que si me apagas, pierdo toda mi información!',
+    imagenes: [
+      require('./assets/ram1.png'),
+      require('./assets/ram2.png'),
+      require('./assets/ram3.png')
+    ]
+  },
+  {
+    nombre: 'DRAM 😁',
+    capacidad: '1 GB - 16 GB',
+    tipo: 'Memoria RAM',
+    velocidad: '1600 MT/s - 3200 MT/s',
+    fecha: '1966',
+    descripcion: 'Me conocen por ser la memoria dinámica que necesita un refresco constante, lo cual es una gran desventaja. Sin embargo, soy barata y con gran capacidad, pero cuidado, porque soy más lenta que la SRAM.',
     imagenes: [
       require('./assets/dram1.jpg'),
       require('./assets/dram2.jpg'),
-      require('./assets/dram3.avif'), 
-    ],
+      require('./assets/dram3.jpg')
+    ]
   },
   {
-    nombre: 'SDRAM',
-    capacidad: '8 GB',
+    nombre: 'SDRAM ⚡',
+    capacidad: '256 MB - 16 GB',
     tipo: 'Memoria RAM',
-    fecha: '1993',
-    ventajas: 'Sincronización con el reloj del sistema.',
-    desventajas: 'Más costosa que DRAM.',
+    velocidad: '66 MHz - 2133 MHz',
+    fecha: '1992',
+    descripcion: 'Trabajo a la par del reloj del sistema, sincronizando todo a la perfección para mejorar el acceso a datos. Aunque, claro, mi consumo de energía es un poco mayor, a parte de que mi costo es mayor al de la DRAM.',
     imagenes: [
       require('./assets/sdram.jpg'),
       require('./assets/sdram2.jpg'),
-      require('./assets/sdram3.png'), 
-    ],
+      require('./assets/sdram3.png')
+    ]
   },
+  {
+    nombre: 'DDR SDRAM 🚀',
+    capacidad: '512 MB a 128 GB',
+    velocidad: '266 MT/s - 6400 MT/s',
+    tipo: 'Memoria RAM',
+    fecha: '1998',
+    descripcion: '¡Velocidad es mi segundo nombre! Conmigo los datos se transfieren en ambos flancos del reloj, duplicando la velocidad de transmisión. Eso sí, no soy tan rápida como DDR4 o DDR5.',
+    imagenes: [
+      require('./assets/ddr1.jpg'),
+      require('./assets/ddr2.jpg'),
+      require('./assets/ddr3.jpg')
+    ]
+  },
+  {
+    nombre: 'RDRAM 🏎️',
+    capacidad: '64 MB - 512 MB',
+    tipo: 'Memoria RAM',
+    velocidad: '400 MHz - 800 MHz',
+    fecha: 'Finales de los 90s',
+    descripcion: 'Soy la memoria de alto rendimiento de mi época. Aunque fui rápida y potente, no soy muy popular debido a mi alto costo y producción de calor. ',
+    imagenes: [
+      require('./assets/rdram1.jpg'),
+      require('./assets/rdram2.jpg'),
+      require('./assets/rdram3.jpg')
+    ]
+  },
+  {
+    nombre: 'SRAM 📚',
+    capacidad: '256 KB',
+    tipo: 'Memoria RAM',
+    velocidad: '1 GHz',
+    fecha: '1963',
+    descripcion: 'Soy una memoria que no necesita refrescarse constantemente como la DRAM. Suelo ser el caché de tu CPU y puedo mantener los datos con bajo consumo de energía por largo tiempo.',
+    imagenes: [
+      require('./assets/sram1.jpg'),
+      require('./assets/sram2.jpg'),
+      require('./assets/sram3.jpg')
+    ]
+  },
+  {
+    nombre: 'ROM 📀',
+    capacidad: '32 KB',
+    tipo: 'Memoria ROM',
+    velocidad: '50 a 150 ns',
+    fecha: '1948',
+    descripcion: 'Mis datos no se pueden modificar fácilmente. Guardo la información esencial, como el firmware, y aunque no me puedas cambiar, siempre estaré lista para leerse.',
+    imagenes: [
+      require('./assets/rom1.jpg'),
+      require('./assets/rom2.jpg'),
+      require('./assets/rom3.jpg')
+    ]
+  },
+  {
+    nombre: 'EEPROM ✏️',
+    capacidad: '1 KB - 128 KB',
+    tipo: 'Memoria ROM',
+    velocidad: '1 - 10 ms',
+    fecha: '1971',
+    descripcion: 'Soy una versión especial de la ROM. Puedo ser borrada y reprogramada eléctricamente, guardo pequeñas cantidades de datos y me reprograman con facilidad. ¡Me encanta adaptarme!',
+    imagenes: [
+      require('./assets/eeprom1.jpg'),
+      require('./assets/eeprom2.jpg'),
+      require('./assets/eeprom3.jpg')
+    ]
+  },
+  {
+    nombre: 'CMOS 💡',
+    capacidad: '64 KB / 1 MB',
+    tipo: 'Memoria de semiconductores',
+    velocidad: '100 ns a 1 ms',
+    fecha: '1963',
+    descripcion: 'Yo controlo el BIOS y la configuración básica de tu sistema. Uso muy poca energía, pero manejo voltajes y puedo mantener la información mientras el dispositivo esté en reposo.',
+    imagenes: [
+      require('./assets/cmos1.jpg'),
+      require('./assets/cmos2.jpg'),
+      require('./assets/cmos3.jpg')
+    ]
+  },
+  {
+    nombre: 'Memoria Caché 🧠',
+    capacidad: '32 KB - 8 MB',
+    tipo: 'Memoria RAM',
+    velocidad: '1 ns - 10 ns',
+    fecha: 'Década de 1980',
+    descripcion: 'Soy la memoria que guarda temporalmente los datos más usados para que el CPU no tenga que ir lejos a buscarlos. Mi trabajo es hacer todo más rápido y eficiente.',
+    imagenes: [
+      require('./assets/cache1.jpg'),
+      require('./assets/cache2.jpg'),
+      require('./assets/cache3.jpg')
+    ]
+  }
 ];
 
 export default function App() {
@@ -44,8 +148,19 @@ export default function App() {
         <Image source={memoria.imagenes[imageIndex]} style={styles.image} />
       </TouchableOpacity>
       <Text style={styles.text}> {memoria.nombre}</Text>
-      <Text style={styles.text}>Capacidad: {memoria.capacidad}</Text>
-      <Text style={styles.text}>Tipo: {memoria.tipo}</Text>
+      <View style={styles.tagsContainer}>
+          <View style={styles.tag}>
+            <Text style={styles.tagText}>{memoria.tipo}</Text>
+          </View>
+          <View style={styles.tag}>
+            <Text style={styles.tagText}>{memoria.capacidad}</Text>
+          </View>
+          <View style={styles.tag}>
+            <Text style={styles.tagText}>{memoria.velocidad}</Text>
+          </View>
+
+        </View>
+
       <TouchableOpacity style={styles.infoButton} onPress={() => setSelectedMemory(memoria)}>
         <Text style={styles.buttonText}>Más información</Text>
       </TouchableOpacity>
@@ -65,10 +180,9 @@ export default function App() {
     <View style={styles.container}>
       {selectedMemory ? (
         <View style={styles.details}>
-          <Text style={styles.detailText}>Nombre: {selectedMemory.nombre}</Text>
+          <Text style={styles.detailText}> {selectedMemory.nombre}</Text>
           <Text style={styles.detailText}>Fecha: {selectedMemory.fecha}</Text>
-          <Text style={styles.detailText}>Ventajas: {selectedMemory.ventajas}</Text>
-          <Text style={styles.detailText}>Desventajas: {selectedMemory.desventajas}</Text>
+          <Text style={styles.detailText}> {selectedMemory.descripcion}</Text>
           <TouchableOpacity style={styles.backButton} onPress={() => setSelectedMemory(null)}>
             <Text style={styles.buttonText}>Regresar</Text>
           </TouchableOpacity>
@@ -116,9 +230,12 @@ const styles = StyleSheet.create({
   },
   image: {
     width: 330,
-    height: 500,
+    height: 600,
+    justifyContent: 'center', 
+    alignItems: 'center',
     borderRadius: 15,
-    marginBottom: 20,
+    marginTop:35,
+    marginBottom: 10,
   },
   text: {
     fontSize: 18,
@@ -128,7 +245,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#C7C7FF',
     padding: 10,
     borderRadius: 8,
-    marginTop: 10,
+    marginTop: 0,
   },
   buttonText: {
     color: '#fff',
@@ -154,5 +271,23 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     marginTop: 20,
+  },
+  
+
+  tagsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+
+  },
+  tag: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', 
+    padding: 5,
+    borderRadius: 20,
+    marginRight: 5,
+  },
+  tagText: {
+    fontSize: 12,
+    fontWeight: 'bold',
   },
 });
